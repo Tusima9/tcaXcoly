6293fc670ac65ad7dbbc4f59fe51b54d689c2d0f
cwhk-4 <57931058+cwhk-4@users.noreply.github.com>
2021-11-20 14:38:08 +0900

    UI アップデート
    ログインするのがめんどくさい
    から、私のアカウントを使っているイェイ
    
    - スタート画面
    - メイン画面
    - パネル
    - シーンの移動

---
+++ TcaXcoly/Assets/Script/SceneManagement/SceneManagerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public void JumpToMain( )
+    {
+        SceneManager.LoadSceneAsync( 2 );
+    }
+
+    public void JumpToScenario( )
+    {
+        SceneManager.LoadSceneAsync( 3 );
+    }
+
+    public void JumpToPuzzle()
+    {
+        SceneManager.LoadSceneAsync( 0 );
+    }
+
+    public void JumpToTitle( )
+    {
+        SceneManager.LoadSceneAsync( 1 );
+    }
+
+}
---
+++ TcaXcoly/Assets/Script/UI/PanelControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PanelControl : MonoBehaviour
+{
+    //パネルをゲットする
+    [SerializeField] private GameObject PanelUI;
+
+    void Start()
+    {
+        ClosePanel( );
+    }
+
+    //パネルを開く
+    public void OpenPanel( )
+    {
+        PanelUI.SetActive( true );
+    }
+
+    //パネルを閉じる
+    public void ClosePanel()
+    {
+        PanelUI.SetActive( false );
+    }
+
+}
