47147c3186935e3805624019820270e46e3d14d8
Tusima9 <20j70049mr@edu.tca.ac.jp>
2021-11-22 09:36:59 +0900

    Add of toda branch

---
+++ TcaXcoly/Assets/Script/Puzzle/UI/SceneManagement/SceneManagerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public void JumpToMain( )
+    {
+        SceneManager.LoadSceneAsync( 1 );
+    }
+
+    public void JumpToScenario( )
+    {
+        SceneManager.LoadSceneAsync( 2 );
+    }
+
+    public void JumpToPuzzle()
+    {
+        SceneManager.LoadSceneAsync( 3 );
+    }
+
+    public void JumpToTitle( )
+    {
+        SceneManager.LoadSceneAsync( 0 );
+    }
+
+}
