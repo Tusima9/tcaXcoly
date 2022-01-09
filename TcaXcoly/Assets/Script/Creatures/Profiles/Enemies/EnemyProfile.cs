76c11b1123d8202b6a2dfe643f77a35ef10d68bf
Kradsat <65266823+Kradsat@users.noreply.github.com>
2022-01-09 21:26:03 +0900

    enemyprofile acript create mennu asset fix

--- TcaXcoly/Assets/Script/Creatures/Profiles/Enemies/EnemyProfile.cs
+++ TcaXcoly/Assets/Script/Creatures/Profiles/Enemies/EnemyProfile.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "EnemyProfile", menuName = "Creature/EnemyProfiles")]
 public class EnemyProfile : Profile
 {
     [Header("Enemy")]
