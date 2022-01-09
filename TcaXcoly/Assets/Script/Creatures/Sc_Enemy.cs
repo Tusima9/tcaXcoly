a1b9e3a3bef4738e669e203dafa3f202f6f27d2c
Kradsat <65266823+Kradsat@users.noreply.github.com>
2022-01-09 21:21:02 +0900

    enemy and players scripts

---
+++ TcaXcoly/Assets/Script/Creatures/Sc_Enemy.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Sc_Enemy : Sc_Creature
+{
+    EnemyProfile myProfile => profile as EnemyProfile;
+
+    public override void Awake()
+    {
+        base.Awake();
+        for (int i = 1; i < myProfile.statspoints + 1; i++)
+        {
+            int random = Random.Range((int)StatType.Attack, (int)StatType.Defense + 1);
+            myPoints[random].Value++;
+        }
+    }
+
+    public override void StartAttack(Sc_Creature target)
+    {
+        transform.DOMoveX(initPos.x - 1, animSpeed);
+        base.StartAttack(target);
+    }
+
+    public override void Death()
+    {
+        base.Death();
+        Sc_EventManager.instance.onWin.Invoke(true);
+    }
+}
